d70e0d07b0566ba0755ceafd595cf203de359111
Will Spencer <willspencer16@gmail.com>
2021-06-23 17:36:43 +0100

    Moved initial lines of code into a function and called it.

--- helloworld_start.py
+++ helloworld_start.py
@@ -1,4 +1,8 @@
 
-print("Hello World")
-name = input("What is your name? ")
-print("Nice to meet you,", name)
+def main():
+    print("Hello World")
+    name = input("What is your name? ")
+    print("Nice to meet you,", name)
+
+if __name__ == "__main__":
+    main()
