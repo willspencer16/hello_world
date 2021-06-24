82e6e297d48d5af44b0d98319a635fdb54b45674
Will Spencer <willspencer16@gmail.com>
2021-06-24 16:16:41 +0100

    Created another for loop, this time one with a break statement.

--- loops_start.py
+++ loops_start.py
@@ -13,15 +13,22 @@ def main():
     # Defining a for loop:
     for x in range(5, 10):
         print(x)
-    # This prints numbers five to nine as Python
+    # This prints numbers 5 to 9 as Python
     # for loops are iterators and x represents the
     # individual value within the range.
 
-    # Using a for loop over a collection
+    # Using a for loop over a collection:
     days=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
     for d in days:
         print(d)
     # This prints each string (d) in the days array.
+
+    # Using a break statement:
+    for x in range(5, 10):
+        if (x==7): break
+        print(x)
+    # This prints 5 and 6, then terminates the
+    # loop as x equals 7.
 
 if __name__ == "__main__":
     main()
