9a77267523305bf40e4b32fea1ac5e6eaa478cc4
Will Spencer <willspencer16@gmail.com>
2021-06-24 17:16:24 +0100

    Imported the math module and used pre-built functions and constants.

---
+++ modules_start.py
@@ -0,0 +1,11 @@
+
+# Importing the math module:
+import math
+
+# Using pre-built functions from the math module:
+print("The square root of 16 is", math.sqrt(16))
+# This uses the math module to find the square root of 16.
+
+#Using pre-built constants from the math module:
+print("Pi is:", math.pi)
+# This prints the value of pi (a constant)
