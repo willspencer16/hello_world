11ce5d048a132cba25f68d6afc6f1c97d48a9049
Will Spencer <willspencer16@gmail.com>
2021-06-24 15:25:37 +0100

    Created a while loop that prints and increments the value of a variable until it equals five.

---
+++ loops_start.py
@@ -0,0 +1,14 @@
+
+def main():
+    x = 0
+
+    # Defining a while loop:
+    while (x<5):
+        print(x)
+        x = x+1
+    # This will print x until its value is 5,
+    # as a while loop executes a block of code
+    # while a particular condition evaluates true.
+
+if __name__ == "__main__":
+    main()
