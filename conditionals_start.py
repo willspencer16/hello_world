a39826601ad7dab78dba7f250c20c6fa6c7cd842
Will Spencer <willspencer16@gmail.com>
2021-06-24 14:38:19 +0100

    Added a single line conditional statement.

--- conditionals_start.py
+++ conditionals_start.py
@@ -1,5 +1,5 @@
 
-# Defining a function with two variables:
+# Defining a function with two variables and a conditional flow:
 def main():
     x, y = 100, 100
 
@@ -12,11 +12,16 @@ def main():
 
     print(st)
 
-# Depending on whether the x variable is
-# greater or less than y, the correct string
-# is printed accordingly. If they are the same,
-# however, the last string is printed, as Python
-# evaluating the first conditional to be false
+# Depending on whether the x variable is greater or less than y, the correct string
+# is printed accordingly. If the variables are the same, however, without the elif,
+# the last string is printed, as Python evaluating the first conditional to be false
+# and defaults to the else.
+
+# Using a conditional statement:
+    st = "x is less than y" if (x < y) else "x is greater than or the same as y"
+    print(st)
+
+# This lets you write a common if else statement all in one line.
 
 if __name__ == "__main__":
     main()
