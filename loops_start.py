b25fc0160420a679db217ae7eb12ff495e54d377
Will Spencer <willspencer16@gmail.com>
2021-06-24 15:42:42 +0100

    Added a for loop

--- loops_start.py
+++ loops_start.py
@@ -10,5 +10,12 @@ def main():
     # as a while loop executes a block of code
     # while a particular condition evaluates true.
 
+    # Defining a for loop:
+    for x in range(5, 10):
+        print(x)
+    # This prints numbers five to nine as Python
+    # for loops are iterators and x represents the
+    # individual value within the range.
+
 if __name__ == "__main__":
     main()
