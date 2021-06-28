4a797e824c4da70ec5aff5f5704208de74d97f76
Will Spencer <willspencer16@gmail.com>
2021-06-28 17:08:21 +0100

    Experimented further with the date object, including using the weekday component.

--- dates_start.py
+++ dates_start.py
@@ -4,10 +4,18 @@ from datetime import date
 from datetime import time
 from datetime import datetime
 
-# Defining a function, calling the import function within:
+# Defining a function, calling the imported function within:
 def main():
     today = date.today()
     print("Today's date is ", today)
+
+    # Extracting specific components:
+    print("Date components: ", today.day, today.month, today.year)
+
+    # Using the weekday component:
+    print("Today's weekday # is: ", today.weekday())
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    print ("Which is a: ", days[today.weekday()])
 
 if __name__ == "__main__":
     main()
