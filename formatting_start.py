62c35297ad5b1d07e9e877a29851213aa7a9b6db
Will Spencer <willspencer16@gmail.com>
2021-06-28 17:48:15 +0100

    Used predifined string control codes to format the time in different ways.

--- formatting_start.py
+++ formatting_start.py
@@ -18,5 +18,9 @@ def main():
     print(now.strftime("locale date: %x"))
     print(now.strftime("locale time: %X"))
 
+    # Using the predefined string control codes to format the time:
+    print(now.strftime("Current time: %I:%M:%S %p"))
+    print(now.strftime("24-hour time: %H:%M"))
+
 if __name__ == "__main__":
     main()
