003dc4c0b78353d103c644e66406543a4bfefc4c
Will Spencer <willspencer16@gmail.com>
2021-06-29 15:33:22 +0100

    Used the timedelta to work out the date this time next year.

---
+++ timedeltas_start.py
@@ -0,0 +1,16 @@
+# Working with timedelta objects:
+
+from datetime import date
+from datetime import time
+from datetime import datetime
+from datetime import timedelta
+
+# Constructing a basic timedelta:
+print(timedelta(days=365, hours=5, minutes=1))
+
+# Printing today's date
+now = datetime.now()
+print("Today is: " + str(now))
+
+# Printing today's date one year from now:
+print("One year from now it will be: " + str(now + timedelta(days=365)))
