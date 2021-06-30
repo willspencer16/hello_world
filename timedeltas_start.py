09a3421a31ebddae5dbcbf0d33ad0bd3704a2429
Will Spencer <willspencer16@gmail.com>
2021-06-30 09:17:29 +0100

    Used the timedelta to work out the date a week ago.

--- timedeltas_start.py
+++ timedeltas_start.py
@@ -17,3 +17,8 @@ print("One year from now it will be: " + str(now + timedelta(days=365)))
 
 # Using a timedelta that has more than one argument:
 print("In two days and three weeks, it will be: " + str(now + timedelta(days=2, weeks=3)))
+
+# Using a timedelta to work out a date from the past:
+t = datetime.now() - timedelta(weeks=1)
+s = t.strftime("%A %B %d, %Y")
+print("One week ago it was: " + s)
