3c67db254121db7d00802d4071d2d9727e49d7dd
Will Spencer <willspencer16@gmail.com>
2021-06-30 10:11:37 +0100

    Iterated over the days of the month from the calendar module.

--- calendars_start.py
+++ calendars_start.py
@@ -11,3 +11,10 @@ hc = calendar.HTMLCalendar(calendar.SUNDAY)
 st = hc.formatmonth(2021, 1)
 print (st)
 # This prints out the html code to create the calendar.
+
+# Looping over the days of a month:
+for i in c.itermonthdays(2021, 8):
+    print(i)
+# This prints of the numbers that represent each day of the month.
+# There are zeros at the start and the end because there are days
+# in that week which belong to another month.
