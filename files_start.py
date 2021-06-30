a90d5c982e3d501d70e859118134e932b1e52c1c
Will Spencer <willspencer16@gmail.com>
2021-06-30 11:29:02 +0100

    Used the read function to read the file content and print it to the console.

--- files_start.py
+++ files_start.py
@@ -8,17 +8,22 @@ def main():
     # '+' creates the file if it doesn't already exist.
 
     # Opening the file for appending text to the end:
-    f = open("textfile.txt", "a")
+    f = open("textfile.txt", "r")
     # The a argument appends the end of the file, instead
     # of overwriting everything that is already in the file.
 
     # Writing some lines of code to the file:
-    for i in range(10):
-        f.write("This is line" + str(i) + "\r\n")
+    # for i in range(10):
+    #     f.write("This is line" + str(i) + "\r\n")
         # The write function writes data to the file.
     
     # Closing the file:
-    f.close()
+    # f.close()
+
+    # Opening the file and reading the contents:
+    if f.mode == 'r': # This checks the file can be read.
+        contents = f.read() # This reads the content.
+        print(contents) # This prints the contents to the console.
 
 
 if __name__ == "__main__":
