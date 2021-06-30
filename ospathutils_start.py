188f60111cb4b4bcc9f1532bec9feabb5e8e21ac
Will Spencer <willspencer16@gmail.com>
2021-06-30 15:15:15 +0100

    Used the OS path module to check file path.

--- ospathutils_start.py
+++ ospathutils_start.py
@@ -15,5 +15,9 @@ def main():
     print("Item is a file: " + str(path.isfile("textfile.txt")))
     print("Item is a directory: " + str(path.isdir("textfile.txt")))
 
+    # Working with file paths:
+    print("Item path: " + str(path.realpath("textfile.txt")))
+    print("Item path and name: " + str(path.split(path.realpath("textfile.txt"))))
+
 if __name__ == "__main__":
     main()
