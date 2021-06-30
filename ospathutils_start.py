6b87789f2ce063d384f52b2e75a27cca189430f9
Will Spencer <willspencer16@gmail.com>
2021-06-30 15:46:32 +0100

    Used the OS path module to calculate how long ago the file was modified.

--- ospathutils_start.py
+++ ospathutils_start.py
@@ -24,5 +24,10 @@ def main():
     print(t)
     print(datetime.datetime.fromtimestamp(path.getmtime("textfile.txt")))
 
+    # Calculating how long ago the file was modified:
+    td = datetime.datetime.now() - datetime.datetime.fromtimestamp(path.getmtime("textfile.txt"))
+    print("It has been " + str(td) + " since the file was modified")
+    print("Or, " + str(td.total_seconds()) + " seconds")
+
 if __name__ == "__main__":
     main()
