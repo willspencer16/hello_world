72d3aff009c2d05c315330b6bc468bf15ba8977f
Will Spencer <willspencer16@gmail.com>
2021-06-30 10:07:18 +0100

    Used the calendar module to create the HTML code for the calendar.

--- calendars_start.py
+++ calendars_start.py
@@ -5,3 +5,9 @@ import calendar
 c = calendar.TextCalendar(calendar.SUNDAY) # Whatever day is specified here, is the day the calendar starts with.
 st = c.formatmonth(2021, 1, 0, 0)
 print (st)
+
+# Creating an HTML formatted calendar:
+hc = calendar.HTMLCalendar(calendar.SUNDAY)
+st = hc.formatmonth(2021, 1)
+print (st)
+# This prints out the html code to create the calendar.
