b08c8145f994f9368a0566ab7add187ef2d396c2
Will Spencer <willspencer16@gmail.com>
2021-06-30 15:00:49 +0100

    Set up imports and printed the name of the OS to the console.

---
+++ ospathutils_start.py
@@ -0,0 +1,14 @@
+# Working with os.path module:
+
+import os
+from os import path
+import datetime
+from datetime import date, time, timedelta
+import time
+
+def main():
+    # Printing the name of the OS:
+    print(os.name)
+
+if __name__ == "__main__":
+    main()
