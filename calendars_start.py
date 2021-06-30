b25a73e5ff274a6fd9adc9a275ff7a39f477bcf3
Will Spencer <willspencer16@gmail.com>
2021-06-30 10:32:22 +0100

    Calculated dates based on a rule, to print out the day of the first friday of each month.

--- calendars_start.py
+++ calendars_start.py
@@ -26,3 +26,19 @@ for name in calendar.month_name:
 for day in calendar.day_name:
     print(day)
 # This varies depending on the locale of the computer.
+
+# Calculating days based on a rule
+print ("Team meetings will be on: ")
+
+for m in range(1,13):
+    cal = calendar.monthcalendar(2022, m)
+    weekone=cal[0]
+    weektwo=cal[1]
+
+    if weekone[calendar.FRIDAY] != 0:
+        meetday = weekone[calendar.FRIDAY]
+    else:
+        meetday = weektwo[calendar.FRIDAY]
+    
+    print("%10s %2d" % (calendar.month_name[m], meetday))
+# This prints out the day of the first friday of each month.
