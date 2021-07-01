117af54a6c07314a1f3520f806d89108b8c2f28e
Will Spencer <willspencer16@gmail.com>
2021-07-01 11:52:33 +0100

    Used the read function to return the html that represents Google's homepage.

--- inetdata_start.py
+++ inetdata_start.py
@@ -4,5 +4,8 @@ def main():
     webUrl = urllib.request.urlopen("http://www.google.co.uk")
     print("Result code: " + str(webUrl.getcode()))
 
+    data = webUrl.read()
+    print(data)
+    
 if __name__ == "__main__":
     main()
